af87298592eaf813feadbdb9ea3e4a302461f0e3
Sim Virdi <simeransvirdi@gmail.com>
2015-10-30 17:58:57 +0000

    Creating User table in db, updating rest of database setup and updating lotsofmenus.py

--- database_setup.py
+++ database_setup.py
@@ -5,11 +5,21 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
+class User(Base):
+  __tablename__ = 'user'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String(250), nullable=False)
+  email = Column(String(250), nullable=False)
+  picture = Column(String(250))
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -30,7 +40,8 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
-
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -43,9 +54,7 @@ class MenuItem(Base):
            'course'         : self.course,
        }
 
-
-
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
  
 
 Base.metadata.create_all(engine)
